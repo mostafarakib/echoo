b5373fa816a1e229c630d8b4d5295bc924d1517e
mostafarakib <mostafa.rakib.10@gmail.com>
2025-10-03 12:32:46 +0600

    added search users methods on backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -51,4 +51,18 @@ const authUser = async (req, res) => {
   }
 };
 
-export { registerUser, authUser };
+// /api/user?search=rakib
+const allUsers = async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+};
+export { registerUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { authUser, registerUser } from "../controllers/userControllers.js";
+import {
+  authUser,
+  registerUser,
+  allUsers,
+} from "../controllers/userControllers.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, allUsers);
 
 router.post("/login", authUser);
 

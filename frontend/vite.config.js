ab9a0d52f27ecdf15a682c0f843cc990b2283610
mostafarakib <mostafa.rakib.10@gmail.com>
2025-11-17 19:28:01 +0600

    file extension import issue on vite build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: [".mjs", ".js", ".jsx", ".json", ".ts", ".tsx"],
-  },
-  build: {
-    rollupOptions: {
-      external: [],
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true,
-    },
-  },
   server: {
     proxy: {
       "/api": "http://127.0.0.1:5000",
